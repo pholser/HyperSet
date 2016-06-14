cd4850c156558b2e8918497fd89ddf96bf444e27
Paul Holser <pholser@alumni.rice.edu>
2016-06-13 20:25:19 -0500

    Adding more to storyboard

--- HyperSet/ViewController.swift
+++ HyperSet/ViewController.swift
@@ -9,7 +9,100 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: Properties
 
+    @IBOutlet weak var card0: UIImageView!
+    @IBOutlet weak var card1: UIImageView!
+    @IBOutlet weak var card2: UIImageView!
+    @IBOutlet weak var card3: UIImageView!
+    @IBOutlet weak var card4: UIImageView!
+    @IBOutlet weak var card5: UIImageView!
+    @IBOutlet weak var card6: UIImageView!
+    @IBOutlet weak var card7: UIImageView!
+    @IBOutlet weak var card8: UIImageView!
+    
+    @IBOutlet weak var card9: UIImageView!
+    @IBOutlet weak var card10: UIImageView!
+    @IBOutlet weak var card11: UIImageView!
+    @IBOutlet weak var card12: UIImageView!
+    @IBOutlet weak var card13: UIImageView!
+    @IBOutlet weak var card14: UIImageView!
+    @IBOutlet weak var card15: UIImageView!
+    @IBOutlet weak var card16: UIImageView!
+    @IBOutlet weak var card17: UIImageView!
+
+    @IBOutlet weak var card18: UIImageView!
+    @IBOutlet weak var card19: UIImageView!
+    @IBOutlet weak var card20: UIImageView!
+    @IBOutlet weak var card21: UIImageView!
+    @IBOutlet weak var card22: UIImageView!
+    @IBOutlet weak var card23: UIImageView!
+    @IBOutlet weak var card24: UIImageView!
+    @IBOutlet weak var card25: UIImageView!
+    @IBOutlet weak var card26: UIImageView!
+    
+    @IBOutlet weak var card27: UIImageView!
+    @IBOutlet weak var card28: UIImageView!
+    @IBOutlet weak var card29: UIImageView!
+    @IBOutlet weak var card30: UIImageView!
+    @IBOutlet weak var card31: UIImageView!
+    @IBOutlet weak var card32: UIImageView!
+    @IBOutlet weak var card33: UIImageView!
+    @IBOutlet weak var card34: UIImageView!
+    @IBOutlet weak var card35: UIImageView!
+    
+    @IBOutlet weak var card36: UIImageView!
+    @IBOutlet weak var card37: UIImageView!
+    @IBOutlet weak var card38: UIImageView!
+    @IBOutlet weak var card39: UIImageView!
+    @IBOutlet weak var card40: UIImageView!
+    @IBOutlet weak var card41: UIImageView!
+    @IBOutlet weak var card42: UIImageView!
+    @IBOutlet weak var card43: UIImageView!
+    @IBOutlet weak var card44: UIImageView!
+    
+    @IBOutlet weak var card45: UIImageView!
+    @IBOutlet weak var card46: UIImageView!
+    @IBOutlet weak var card47: UIImageView!
+    @IBOutlet weak var card48: UIImageView!
+    @IBOutlet weak var card49: UIImageView!
+    @IBOutlet weak var card50: UIImageView!
+    @IBOutlet weak var card51: UIImageView!
+    @IBOutlet weak var card52: UIImageView!
+    @IBOutlet weak var card53: UIImageView!
+    
+    @IBOutlet weak var card54: UIImageView!
+    @IBOutlet weak var card55: UIImageView!
+    @IBOutlet weak var card56: UIImageView!
+    @IBOutlet weak var card57: UIImageView!
+    @IBOutlet weak var card58: UIImageView!
+    @IBOutlet weak var card59: UIImageView!
+    @IBOutlet weak var card60: UIImageView!
+    @IBOutlet weak var card61: UIImageView!
+    @IBOutlet weak var card62: UIImageView!
+    
+    @IBOutlet weak var card63: UIImageView!
+    @IBOutlet weak var card64: UIImageView!
+    @IBOutlet weak var card65: UIImageView!
+    @IBOutlet weak var card66: UIImageView!
+    @IBOutlet weak var card67: UIImageView!
+    @IBOutlet weak var card68: UIImageView!
+    @IBOutlet weak var card69: UIImageView!
+    @IBOutlet weak var card70: UIImageView!
+    @IBOutlet weak var card71: UIImageView!
+    
+    @IBOutlet weak var card72: UIImageView!
+    @IBOutlet weak var card73: UIImageView!
+    @IBOutlet weak var card74: UIImageView!
+    @IBOutlet weak var card75: UIImageView!
+    @IBOutlet weak var card76: UIImageView!
+    @IBOutlet weak var card77: UIImageView!
+    @IBOutlet weak var card78: UIImageView!
+    @IBOutlet weak var card79: UIImageView!
+    @IBOutlet weak var card80: UIImageView!
+    
+    @IBOutlet weak var generateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
